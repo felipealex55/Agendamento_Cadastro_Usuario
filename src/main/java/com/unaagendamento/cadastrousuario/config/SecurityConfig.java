632a82c72494121caf750f797660a7aab6ad2c40
Felipe Alexsander <felipealexsander05@gmail.com>
2025-11-16 09:10:56 -0300

    Alteracao nome pasta config

--- src/main/java/com/unaagendamento/cadastrousuario/config/SecurityConfig.java
+++ src/main/java/com/unaagendamento/cadastrousuario/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.unaagendamento.cadastrousuario.confing;
+package com.unaagendamento.cadastrousuario.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
